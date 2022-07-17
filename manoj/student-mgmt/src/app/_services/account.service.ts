fff7beccb64c6a77c8568faaf00e15d8a4b460fb
manoj97738 <manoj97738@gmail.com>
2022-07-17 17:41:30 +0530

    Bugfixes

--- manoj/student-mgmt/src/app/_services/account.service.ts
+++ manoj/student-mgmt/src/app/_services/account.service.ts
@@ -1,24 +1,15 @@
 ﻿import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
 
 
 @Injectable({ providedIn: "root" })
 export class AccountService {
-  constructor(
-    private http: HttpClient
-  ) {
-
-  }
+  constructor(private http: HttpClient) { }
   login(username: string, password: string) {
     return this.http.post(`http://localhost:8080/api/auth/signin`, { username: username, password: password })
-
   }
   signup(email: string, password: string, username: string) {
     return this.http.post(`http://localhost:8080/api/auth/signup`, { username, password, email })
-
   }
 }
